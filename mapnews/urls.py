97fb6f49bfc8ab51193210b3d737b2caec6c710e
Roy Yali <ryali93@gmail.com>
2019-10-03 07:47:59 -0500

    arreglos visuales

--- mapnews/urls.py
+++ mapnews/urls.py
@@ -11,7 +11,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index1.html'), name='home'),
     # url(r'^data.geojson$', GeoJSONLayerView.as_view(model=MapnewsHotspot, properties=('title', 'description', 'picture_url')), name='data'),
     url(r'^data.geojson$', GeoJSONLayerView.as_view(model=MapnewsHotspot, properties=('title', 'description','dia')), name='data')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
